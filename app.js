5070b07eebc9341ccec7335bd6625cd4e1558a32
Danny Pifer <piferdg@gmail.com>
2018-07-24 11:57:22 -0600

    Adding working code

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express = require('express')
 const cors = require('cors')
-const data = require('./dataSet')
+const data = require('./data')
 const port = parseInt(process.env.PORT || 8000)
 
 const app = express()
 app.use(cors())
 
-function name(params) {
-  
+function instructorID(data, id) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i]
+    }
+  }
+  return null
 }
+
+app.get('/', (request, response) => {
+  response.json({ data })
+})
+
+app.get('/:id', (request, response) => {
+  let instructor = instructorID(data, request.params.id)
+  
+  if (!instructor) {
+    response.status(400).json({
+      error: {
+        message: 'No record found!'
+      }
+    })
+  } else {
+    response.json({
+      data: instructor
+    })
+  }
+})
+
+app.listen(port, () =>
+  console.log('listening on port', port))
---
+++ data.js
@@ -0,0 +1,61 @@
+module.exports = [{
+  id: 1,
+  fullName: 'Kyle Coberly',
+  title: 'Faculty Director',
+  dogs: 0
+},{
+  id: 2,
+  fullName: 'Danny Fritz',
+  title: 'Lead Instructor',
+  dogs: 0
+},{
+  id: 3,
+  fullName: 'CJ Reynolds',
+  title: 'Lead Instructor',
+  dogs: 0
+},{
+  id: 4,
+  fullName: 'Brooks Patton',
+  title: 'Lead Instructor',
+  dogs: 0
+},{
+  id: 5,
+  fullName: 'Roberto Ortega',
+  title: 'Lead Instructor',
+  dogs: 1
+},{
+  id: 6,
+  fullName: 'Chad Drummond',
+  title: 'Instructor',
+  dogs: 0
+},{
+  id: 7,
+  fullName: 'Kim Schlesinger',
+  title: 'Instructor',
+  dogs: 0
+},{
+  id: 8,
+  fullName: 'Peter Ostiguy',
+  title: 'Associate Instructor',
+  dogs: 1
+},{
+  id: 9,
+  fullName: 'Cass Torske',
+  title: 'Resident',
+  dogs: 1
+},{
+  id: 10,
+  fullName: 'Matt Winzer',
+  title: 'Resident',
+  dogs: 2
+},{
+  id: 11,
+  fullName: 'Aaron Goodman',
+  title: 'Resident',
+  dogs: 0
+},{
+  id: 12,
+  fullName: 'Michelle Bergquist',
+  title: 'Resident',
+  dogs: 1
+}]
