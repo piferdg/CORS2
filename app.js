100c087cf7932d1635be0875c142e7de92fc5366
Danny Pifer <piferdg@gmail.com>
2018-07-24 13:31:54 -0600

    Adding an edit to app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/:id', (request, response) => {
   let instructor = instructorID(data, request.params.id)
   
   if (!instructor) {
-    response.status(400).json({
+    response.status(404).json({
       error: {
         message: 'No record found!'
       }
